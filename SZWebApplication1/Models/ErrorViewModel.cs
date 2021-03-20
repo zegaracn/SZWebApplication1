b267ac7e8c1086167db958dd2267cde969ea77bd
zegaracn <zegaracn@yahoo.com.au>
2021-03-20 22:40:56 +1100

    Add project files.

---
+++ SZWebApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SZWebApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
